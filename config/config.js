1b0f53a32b605b95f8c0fd9069d97fa1dd85e274
Hendrik <55944332+berenbums@users.noreply.github.com>
2020-09-22 11:08:12 +0200

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,60 @@
+module.exports = {
+    development: {
+      username: 'root',
+      password: process.env.SQLPASS,
+      database: 'appbeware_dev',
+      details: {
+        host: '127.0.0.1',
+        port: 3306,
+        dialect: 'mysql'
+      }
+    },
+    // test: {
+    //   username: process.env.TU,
+    //   password: process.env.TP || null,
+    //   database: 'appbeware_test',
+    //   details: {
+    //     host: 'localhost',
+    //     port: 3306,
+    //     dialect: 'mysql',
+    //     logging: false
+    //   }
+    // },
+
+    // development: {
+    //   username: process.env.RDS_USERNAME,
+    //   password: process.env.RDS_PASSWORD,
+    //   database: process.env.RDS_DB_NAME,
+    //   // 'use_env_variable': 'JAWSDB_URL',
+    //   details: {
+    //     host: process.env.RDS_HOSTNAME,
+    //     port: process.env.RDS_PORT,
+    //     dialect: 'mysql'
+    //   }
+    // },
+
+    test: {
+      username: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      database: process.env.RDS_DB_NAME,
+      // 'use_env_variable': 'JAWSDB_URL',
+      details: {
+        host: process.env.RDS_HOSTNAME,
+        port: process.env.RDS_PORT,
+        dialect: 'mysql'
+      }
+    },
+
+
+    production: {
+      username: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      database: process.env.RDS_DB_NAME,
+      // 'use_env_variable': 'JAWSDB_URL',
+      details: {
+        host: process.env.RDS_HOSTNAME,
+        port: process.env.RDS_PORT,
+        dialect: 'mysql'
+      }
+    }
+  };
